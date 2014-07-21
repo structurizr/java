6e80c6201d495504cde3118e6d9849b3a4867b1e
Simon Brown <simon.brown@codingthearchitecture.com>
2014-07-21 08:31:59 +0100

    More minor tweaks.

--- core/src/com/structurizr/model/Model.java
+++ core/src/com/structurizr/model/Model.java
@@ -18,8 +18,27 @@ public class Model {
     private String name;
     private String description;
 
+    public Model() {
+    }
+
     public Model (String name, String description) {
         this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 
