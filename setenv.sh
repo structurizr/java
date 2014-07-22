40ce9a6c5a6f77ae8593f3875fdfb03b1bee9efd
Simon Brown <simon.brown@codingthearchitecture.com>
2014-07-22 11:27:01 +0100

    Cleaned up the Ant/Ivy build.

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,3 @@
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home/
-export ANT_HOME=~//dev/apache-ant-1.9.1
+export ANT_HOME=~/dev/apache-ant-1.9.1
 export PATH=$ANT_HOME/bin:$PATH
