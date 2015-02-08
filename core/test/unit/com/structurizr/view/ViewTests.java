541de0da43acf5eb44ee9d6e656328955afc8e18
Simon Brown <simon.brown@codingthearchitecture.com>
2015-02-08 13:07:11 +0000

    Extended the test.

--- core/test/unit/com/structurizr/view/ViewTests.java
+++ core/test/unit/com/structurizr/view/ViewTests.java
@@ -181,23 +181,33 @@ public class ViewTests extends AbstractWorkspaceTestBase {
     @Test
     public void test_copyLayoutInformationFrom() {
         SoftwareSystem softwareSystem = model.addSoftwareSystem(Location.Internal, "The System", "Description");
+        SoftwareSystem softwareSystemA = model.addSoftwareSystem(Location.Unspecified, "System A", "Description");
+        SoftwareSystem softwareSystemB = model.addSoftwareSystem(Location.Unspecified, "System B", "Description");
         Person person = model.addPerson(Location.Unspecified, "Person", "Description");
 
+        // create a view with SystemA and Person (locations are set for both)
         View view1 = new SystemContextView(softwareSystem);
-        view1.addAllSoftwareSystems();
-        view1.addAllPeople();
-        view1.findElementView(person).setX(123);
-        view1.findElementView(person).setY(456);
+        view1.addSoftwareSystem(softwareSystemA);
+        view1.findElementView(softwareSystemA).setX(123);
+        view1.findElementView(softwareSystemA).setY(321);
+        view1.addPerson(person);
+        view1.findElementView(person).setX(456);
+        view1.findElementView(person).setY(654);
 
+        // create a view with SystemB and Person (locations are 0,0 for both)
         View view2 = new SystemContextView(softwareSystem);
-        view2.addAllSoftwareSystems();
-        view2.addAllPeople();
-
+        view2.addSoftwareSystem(softwareSystemB);
+        view2.addPerson(person);
+        assertEquals(0, view2.findElementView(softwareSystemB).getX());
+        assertEquals(0, view2.findElementView(softwareSystemB).getY());
         assertEquals(0, view2.findElementView(person).getX());
         assertEquals(0, view2.findElementView(person).getY());
+
         view2.copyLayoutInformationFrom(view1);
-        assertEquals(123, view2.findElementView(person).getX());
-        assertEquals(456, view2.findElementView(person).getY());
+        assertEquals(0, view2.findElementView(softwareSystemB).getX());
+        assertEquals(0, view2.findElementView(softwareSystemB).getY());
+        assertEquals(456, view2.findElementView(person).getX());
+        assertEquals(654, view2.findElementView(person).getY());
     }
 
 }
