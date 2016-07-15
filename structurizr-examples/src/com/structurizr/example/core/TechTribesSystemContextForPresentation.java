a3eca07fd7a8431a0ceb35da44a68ffc02608f9e
Simon Brown <simon.brown@codingthearchitecture.com>
2016-07-15 14:13:09 +0100

    Removed locations.

--- structurizr-examples/src/com/structurizr/example/core/TechTribesSystemContextForPresentation.java
+++ structurizr-examples/src/com/structurizr/example/core/TechTribesSystemContextForPresentation.java
@@ -16,26 +16,26 @@ public class TechTribesSystemContextForPresentation {
         Model model = workspace.getModel();
 
         // create a model and the software system we want to describe
-        SoftwareSystem techTribes = model.addSoftwareSystem(Location.Internal, "techtribes.je", "techtribes.je is the only way to keep up to date with the IT, tech and digital sector in Jersey and Guernsey, Channel Islands");
+        SoftwareSystem techTribes = model.addSoftwareSystem("techtribes.je", "techtribes.je is the only way to keep up to date with the IT, tech and digital sector in Jersey and Guernsey, Channel Islands");
 
         // create the various types of people (roles) that use the software system
-        Person anonymousUser = model.addPerson(Location.External, "Anonymous User", "Anybody on the web.");
+        Person anonymousUser = model.addPerson("Anonymous User", "Anybody on the web.");
         anonymousUser.uses(techTribes, "View people, tribes (businesses, communities and interest groups), content, events, jobs, etc from the local tech, digital and IT sector.");
 
-        Person authenticatedUser = model.addPerson(Location.External, "Aggregated User", "A user or business with content aggregated into the website.");
+        Person authenticatedUser = model.addPerson("Aggregated User", "A user or business with content aggregated into the website.");
         authenticatedUser.uses(techTribes, "Manage user profile and tribe membership.");
 
-        Person adminUser = model.addPerson(Location.External, "Administration User", "A system administration user.");
+        Person adminUser = model.addPerson("Administration User", "A system administration user.");
         adminUser.uses(techTribes, "Add people, add tribes and manage tribe membership.");
 
         // create the various software systems that techtribes.je has a dependency on
-        SoftwareSystem twitter = model.addSoftwareSystem(Location.External, "Twitter", "twitter.com");
+        SoftwareSystem twitter = model.addSoftwareSystem("Twitter", "twitter.com");
         techTribes.uses(twitter, "Gets profile information and tweets from.");
 
-        SoftwareSystem gitHub = model.addSoftwareSystem(Location.External, "GitHub", "github.com");
+        SoftwareSystem gitHub = model.addSoftwareSystem("GitHub", "github.com");
         techTribes.uses(gitHub, "Gets information about public code repositories from.");
 
-        SoftwareSystem blogs = model.addSoftwareSystem(Location.External, "Blogs", "RSS and Atom feeds");
+        SoftwareSystem blogs = model.addSoftwareSystem("Blogs", "RSS and Atom feeds");
         techTribes.uses(blogs, "Gets content using RSS and Atom feeds from.");
 
         // now create the system context view based upon the model
