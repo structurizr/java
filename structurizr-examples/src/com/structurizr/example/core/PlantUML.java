7ef052c83e1e23dc9476cd7d8059818cbf7b9d49
Simon Brown <simon.brown@codingthearchitecture.com>
2017-07-03 15:31:47 +0100

    Added a simple PlantUML example.

---
+++ structurizr-examples/src/com/structurizr/example/core/PlantUML.java
@@ -0,0 +1,37 @@
+package com.structurizr.example.core;
+
+import com.structurizr.Workspace;
+import com.structurizr.io.plantuml.PlantUMLWriter;
+import com.structurizr.model.Model;
+import com.structurizr.model.Person;
+import com.structurizr.model.SoftwareSystem;
+import com.structurizr.view.SystemContextView;
+import com.structurizr.view.ViewSet;
+
+import java.io.StringWriter;
+
+/**
+ * An example of how to use the PlantUML writer.
+ */
+public class PlantUML {
+
+    public static void main(String[] args) throws Exception {
+        Workspace workspace = new Workspace("Getting Started", "This is a model of my software system.");
+        Model model = workspace.getModel();
+
+        Person user = model.addPerson("User", "A user of my software system.");
+        SoftwareSystem softwareSystem = model.addSoftwareSystem("Software System", "My software system.");
+        user.uses(softwareSystem, "Uses");
+
+        ViewSet viewSet = workspace.getViews();
+        SystemContextView contextView = viewSet.createSystemContextView(softwareSystem, "context", "An example of a System Context diagram.");
+        contextView.addAllSoftwareSystems();
+        contextView.addAllPeople();
+
+        StringWriter stringWriter = new StringWriter();
+        PlantUMLWriter plantUMLWriter = new PlantUMLWriter();
+        plantUMLWriter.write(workspace, stringWriter);
+        System.out.println(stringWriter.toString());
+    }
+
+}
