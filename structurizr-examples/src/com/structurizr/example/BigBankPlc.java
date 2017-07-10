a1b83918d002f6b87b3ad1673ae73b7fabe28f37
Simon Brown <simon.brown@codingthearchitecture.com>
2017-07-10 18:09:34 +0100

    Switched to embedded branding logo.

--- structurizr-examples/src/com/structurizr/example/BigBankPlc.java
+++ structurizr-examples/src/com/structurizr/example/BigBankPlc.java
@@ -5,8 +5,12 @@ import com.structurizr.api.StructurizrClient;
 import com.structurizr.documentation.Format;
 import com.structurizr.documentation.StructurizrDocumentation;
 import com.structurizr.model.*;
+import com.structurizr.util.ImageUtils;
 import com.structurizr.util.MapUtils;
 import com.structurizr.view.*;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * This is an example workspace to illustrate the key features of Structurizr,
@@ -137,8 +141,12 @@ public class BigBankPlc {
             liveDeploymentView.add(dataReplicationRelationship);
             liveDeploymentView.setPaperSize(PaperSize.A5_Landscape);
 
-            Branding branding = views.getConfiguration().getBranding();
-            branding.setLogo("https://structurizr.com/static/img/structurizr-logo.png");
+            try {
+                Branding branding = views.getConfiguration().getBranding();
+                branding.setLogo(ImageUtils.getImageAsDataUri(new File("./docs/images/structurizr-logo.png")));
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
         }
 
         // colours, shapes and other diagram styling
