4f9a0b2ad3356523a5571e1b3996cc1905399f94
Mattias Jiderhamn <mattias.jiderhamn@readsoft.com>
2018-03-23 08:42:06 +0100

    Add NormalizedNameIdGenerator for generating IDs based on (canonical) name

---
+++ structurizr-core/src/com/structurizr/model/NormalizedNameIdGenerator.java
@@ -0,0 +1,66 @@
+package com.structurizr.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * An ID generator that uses the element name to generate IDs for model elements and relationships.
+ * The canonical name will be normalized by removing any characters other than letters and numbers.
+ */
+public class NormalizedNameIdGenerator implements IdGenerator {
+    
+    private final boolean canonical;
+
+    private Set<String> usedIds = new HashSet<>();
+
+    /** 
+     * Use the {@link Element#getCanonicalName()}.
+     */
+    public NormalizedNameIdGenerator() {
+        this(true);
+    }
+
+    /** 
+     * @param canonical {@code true} will use the {@link Element#getCanonicalName()}, {@code false} will use {@link Element#getName()}
+     */
+    public NormalizedNameIdGenerator(boolean canonical) {
+        this.canonical = canonical;
+    }
+
+    @Override
+    public void found(String id) {
+        usedIds.add(id); // Remember as used to avoid clashes
+    }
+
+    @Override
+    public String generateId(Element element) {
+        if(! canonical && element instanceof ContainerInstance) { // Special treatment
+            ContainerInstance containerInstance = (ContainerInstance) element;
+            return generateId(containerInstance.getContainerId() + containerInstance.getInstanceId());
+        }
+        else 
+            return generateId(canonical ? element.getCanonicalName() : element.getName());
+    }
+
+    @Override
+    public String generateId(Relationship relationship) {
+        return generateId(
+            canonical ? relationship.getSource().getCanonicalName() : relationship.getSource().getName(),
+            relationship.getDescription(),
+            canonical ? relationship.getDestination().getCanonicalName() : relationship.getDestination().getName());
+    }
+
+    private String generateId(final String...terms) {
+        final StringBuilder sb = new StringBuilder();
+        for (final String term : terms) {
+            if(term!=null) {
+                sb.append(term.replaceAll("[^a-zA-Z0-9]", ""));
+            }
+        }
+
+        final String id = sb.toString();
+        if(! usedIds.add(id))
+            throw new IllegalArgumentException("Non-unique ID generated: " + id);
+        return id;
+    }
+}
