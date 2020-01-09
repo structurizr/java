0b722f4a72dabe568d99a45dd2e304aeb8ea57bb
Simon Brown <simon@structurizr.com>
2020-01-09 13:33:39 +0000

    Added a ThemeUtils class to export element/relationship styles from an existing workspace as a theme.

---
+++ structurizr-client/src/com/structurizr/util/ThemeUtils.java
@@ -0,0 +1,69 @@
+package com.structurizr.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.structurizr.Workspace;
+import com.structurizr.io.WorkspaceWriterException;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Some utility methods for exporting themes to JSON.
+ */
+public final class ThemeUtils {
+
+    /**
+     * Serializes the theme (element and relationship styles) in the specified workspace to a file, as a JSON string.
+     *
+     * @param workspace     a Workspace object
+     * @param file          a File representing the JSON definition
+     * @throws Exception    if something goes wrong
+     */
+    public static void toJson(Workspace workspace, File file) throws Exception {
+        if (workspace == null) {
+            throw new IllegalArgumentException("A workspace must be provided.");
+        } else if (file == null) {
+            throw new IllegalArgumentException("The path to a file must be specified.");
+        }
+
+        OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8);
+        write(workspace, writer);
+    }
+
+    /**
+     * Serializes the theme (element and relationship styles) in the specified workspace to a JSON string.
+     *
+     * @param workspace     a Workspace instance
+     * @return              a JSON string
+     * @throws Exception    if something goes wrong
+     */
+    public static String toJson(Workspace workspace) throws Exception {
+        if (workspace == null) {
+            throw new IllegalArgumentException("A workspace must be provided.");
+        }
+
+        StringWriter writer = new StringWriter();
+        write(workspace, writer);
+
+        return writer.toString();
+    }
+
+    private static void write(Workspace workspace, Writer writer) throws Exception {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+            writer.write(objectMapper.writeValueAsString(workspace.getViews().getConfiguration().getStyles()));
+        } catch (IOException ioe) {
+            throw new WorkspaceWriterException("Could not write the theme as JSON", ioe);
+        }
+
+        writer.flush();
+        writer.close();
+    }
+
+}
