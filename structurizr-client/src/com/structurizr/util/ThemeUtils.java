65016649d91e8c70e8183c11da76c953d9e0a5b9
Simon Brown <simon@structurizr.com>
2020-05-23 21:33:42 +0100

    Adds a name and description to the theme.

--- structurizr-client/src/com/structurizr/util/ThemeUtils.java
+++ structurizr-client/src/com/structurizr/util/ThemeUtils.java
@@ -1,13 +1,18 @@
 package com.structurizr.util;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.structurizr.Workspace;
 import com.structurizr.io.WorkspaceWriterException;
+import com.structurizr.view.ElementStyle;
+import com.structurizr.view.RelationshipStyle;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.LinkedList;
 
 /**
  * Some utility methods for exporting themes to JSON.
@@ -57,13 +62,54 @@ public final class ThemeUtils {
             objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
             objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
 
-            writer.write(objectMapper.writeValueAsString(workspace.getViews().getConfiguration().getStyles()));
+            writer.write(objectMapper.writeValueAsString(
+                    new Theme(
+                            workspace.getName(),
+                            workspace.getDescription(),
+                            workspace.getViews().getConfiguration().getStyles().getElements(),
+                            workspace.getViews().getConfiguration().getStyles().getRelationships()
+                    )));
         } catch (IOException ioe) {
             throw new WorkspaceWriterException("Could not write the theme as JSON", ioe);
         }
 
         writer.flush();
         writer.close();
+    }
+
+    static class Theme {
+
+        private String name;
+        private String description;
+        private Collection<ElementStyle> elements = new LinkedList<>();
+        private Collection<RelationshipStyle> relationships = new LinkedList<>();
+
+        Theme(String name, String description, Collection<ElementStyle> elements, Collection<RelationshipStyle> relationships) {
+            this.name = name;
+            this.description = description;
+            this.elements = elements;
+            this.relationships = relationships;
+        }
+
+        @JsonGetter
+        public String getName() {
+            return name;
+        }
+
+        @JsonGetter
+        public String getDescription() {
+            return description;
+        }
+
+        @JsonGetter
+        public Collection<ElementStyle> getElements() {
+            return elements;
+        }
+
+        @JsonGetter
+        public Collection<RelationshipStyle> getRelationships() {
+            return relationships;
+        }
     }
 
 }
