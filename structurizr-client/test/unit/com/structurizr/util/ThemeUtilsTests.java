750f56ff1011b1b38e258b853d97d93bae377fbf
Simon Brown <simon@structurizr.com>
2020-07-09 10:52:07 +0100

    Added a test for the theme serialisation problem.

--- structurizr-client/test/unit/com/structurizr/util/ThemeUtilsTests.java
+++ structurizr-client/test/unit/com/structurizr/util/ThemeUtilsTests.java
@@ -1,9 +1,11 @@
 package com.structurizr.util;
 
 import com.structurizr.Workspace;
+import com.structurizr.model.Tags;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class ThemeUtilsTests {
 
@@ -16,6 +18,25 @@ public class ThemeUtilsTests {
 
         assertTrue(workspace.getViews().getConfiguration().getStyles().getElements().size() > 0);
         assertTrue(workspace.getViews().getConfiguration().getStyles().getElements().stream().anyMatch(s -> s.getTag().equals("Amazon Web Services - Cloud")));
+    }
+
+    @Test
+    public void test_toJson() throws Exception {
+        Workspace workspace = new Workspace("Name", "Description");
+        assertEquals("{ }", ThemeUtils.toJson(workspace));
+
+        workspace.getViews().getConfiguration().getStyles().addElementStyle(Tags.ELEMENT).background("#ff0000");
+        workspace.getViews().getConfiguration().getStyles().addRelationshipStyle(Tags.RELATIONSHIP).color("#ff0000");
+        assertEquals("{\n" +
+                "  \"elements\" : [ {\n" +
+                "    \"tag\" : \"Element\",\n" +
+                "    \"background\" : \"#ff0000\"\n" +
+                "  } ],\n" +
+                "  \"relationships\" : [ {\n" +
+                "    \"tag\" : \"Relationship\",\n" +
+                "    \"color\" : \"#ff0000\"\n" +
+                "  } ]\n" +
+                "}", ThemeUtils.toJson(workspace));
     }
 
 }
