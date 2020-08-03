43609cc7d0f5f2227b5220728fc041b0fa59c1e6
Simon Brown <simon@structurizr.com>
2020-08-03 22:23:41 +0100

    Prevents failing when enums can't be deserialized.

--- structurizr-client/src/com/structurizr/io/json/AbstractJsonReader.java
+++ structurizr-client/src/com/structurizr/io/json/AbstractJsonReader.java
@@ -9,6 +9,7 @@ class AbstractJsonReader {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
 
         return objectMapper;
     }
