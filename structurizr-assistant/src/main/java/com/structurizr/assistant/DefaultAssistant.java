0e6a2355dc7690d227642bd62b1161b533bc463f
Simon Brown <simon@structurizr.com>
2024-01-11 15:31:01 +0000

    Adds an inspection for when element metadata is hidden via an element style.

--- structurizr-assistant/src/main/java/com/structurizr/assistant/DefaultAssistant.java
+++ structurizr-assistant/src/main/java/com/structurizr/assistant/DefaultAssistant.java
@@ -3,10 +3,12 @@ package com.structurizr.assistant;
 import com.structurizr.Workspace;
 import com.structurizr.assistant.model.*;
 import com.structurizr.assistant.view.ContainerViewsForMultipleSoftwareSystemsInspection;
+import com.structurizr.assistant.view.ElementStyleMetadataInspection;
 import com.structurizr.assistant.view.EmptyViewsInspection;
 import com.structurizr.assistant.view.SystemContextViewsForMultipleSoftwareSystemsInspection;
 import com.structurizr.assistant.workspace.WorkspaceScopeInspection;
 import com.structurizr.model.*;
+import com.structurizr.view.ElementStyle;
 
 public class DefaultAssistant extends Assistant {
 
@@ -62,6 +64,10 @@ public class DefaultAssistant extends Assistant {
         add(new EmptyViewsInspection(workspace).run());
         add(new SystemContextViewsForMultipleSoftwareSystemsInspection(workspace).run());
         add(new ContainerViewsForMultipleSoftwareSystemsInspection(workspace).run());
+
+        for (ElementStyle elementStyle : workspace.getViews().getConfiguration().getStyles().getElements()) {
+            add(new ElementStyleMetadataInspection(workspace).run(elementStyle));
+        }
     }
 
 }
