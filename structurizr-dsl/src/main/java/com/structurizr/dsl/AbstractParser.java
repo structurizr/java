2e9274f6f8be498bd30788c196d4271c804222a2
Simon Brown <simon@structurizr.com>
2025-08-03 08:47:46 +0100

    Adds missing tests, fixes typo in message.

--- structurizr-dsl/src/main/java/com/structurizr/dsl/AbstractParser.java
+++ structurizr-dsl/src/main/java/com/structurizr/dsl/AbstractParser.java
@@ -4,23 +4,11 @@ import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.core5.http.ContentType;
-import org.apache.hc.core5.http.Header;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
-
-import java.util.regex.Pattern;
 
 abstract class AbstractParser {
 
     private static final int HTTP_OK_STATUS = 200;
-
-    private static final Pattern VIEW_KEY_PATTERN = Pattern.compile("[\\w-]+");
-
-    void validateViewKey(String key) {
-        if (!VIEW_KEY_PATTERN.matcher(key).matches()) {
-            throw new RuntimeException("View keys can only contain the following characters: a-zA-0-9_-");
-        }
-    }
 
     String removeNonWordCharacters(String name) {
         return name.replaceAll("\\W", "");
